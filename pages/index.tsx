5a8d1872aa308ac6832c541e2ac3b9ad0b72cf93
apoorvlathey <latheyapoorv@gmail.com>
2022-06-14 00:28:05 +0530

    add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,41 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Lens share</title>
         <link rel="icon" href="/leaf.png" />
+
+        {/* <!-- Primary Meta Tags --> */}
+        <title>Lens share</title>
+        <meta name="title" content="Lens share" />
+        <meta
+          name="description"
+          content="Move your Tweets to the Lens Protocol 🌿"
+        />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://lens-share.apoorv.xyz/" />
+        <meta property="og:title" content="Lens share" />
+        <meta
+          property="og:description"
+          content="Move your Tweets to the Lens Protocol 🌿"
+        />
+        <meta
+          property="og:image"
+          content="https://lens-share.apoorv.xyz/metaIMG.png"
+        />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://lens-share.apoorv.xyz/" />
+        <meta property="twitter:title" content="Lens share" />
+        <meta
+          property="twitter:description"
+          content="Move your Tweets to the Lens Protocol 🌿"
+        />
+        <meta
+          property="twitter:image"
+          content="https://lens-share.apoorv.xyz/metaIMG.png"
+        />
       </Head>
       <Flex flexDir={"column"} minH="100vh">
         <Navbar />
