1bae804720cd1844c04a2e853d7969787855d0e9
apoorvlathey <latheyapoorv@gmail.com>
2022-06-13 06:47:07 +0530

    pass tweet url via url params

--- components/Body/index.tsx
+++ components/Body/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
 import {
-  chakra,
   Box,
   Center,
   Text,
@@ -15,7 +15,7 @@ import {
   Link,
   Heading,
 } from "@chakra-ui/react";
-import { useAccount, useProvider } from "wagmi";
+import { useAccount } from "wagmi";
 import axios from "axios";
 const ConnectWallet = dynamic(() => import("./ConnectWallet"), {
   ssr: false,
@@ -26,6 +26,8 @@ import { htmlDecode } from "../../utils/helpers";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 
 function Body() {
+  const router = useRouter();
+  const tweetURL = router.query.url as string;
   const { data: account } = useAccount();
   const [isFetching, setIsFetching] = useState(false);
   const [tweetText, setTweetText] = useState<string>();
@@ -40,49 +42,58 @@ function Body() {
   } = useLens();
 
   const onPaste = async (e: React.ClipboardEvent) => {
-    setIsFetching(true);
     const items = e.clipboardData.items;
 
     for (const index in items) {
       const item = items[index];
 
       if (item.kind === "string" && item.type === "text/plain") {
-        const tweetURL = e.clipboardData.getData("Text");
-        const _tweetId = tweetURL.split("/").pop()?.split("?")[0];
-
-        try {
-          const res = await axios(`/api/getTweet/${_tweetId}`);
-          const { data: resData } = res;
-          let _tweetText = htmlDecode(resData.data.text);
-
-          let _tweetMedia;
-          if (resData.includes) {
-            _tweetMedia = resData.includes.media.filter(
-              (m: { type: "photo" | "video" }) => m.type === "photo"
-            );
-
-            // remove t.co link from tweet text
-            _tweetText = _tweetText
-              .split("https://t.co/")
-              .slice(0, -1)
-              .join("");
-          }
-
-          console.log({ _tweetText, _tweetMedia });
-          setTweetText(_tweetText);
-          setTweetMedia(_tweetMedia);
-        } catch (e: any) {
-          console.log(e);
-        }
+        const _tweetURL = e.clipboardData.getData("Text");
+        fetchTweetInfoById(_tweetURL);
       }
+    }
+  };
+
+  const fetchTweetInfoById = async (_tweetURL: string) => {
+    setIsFetching(true);
+
+    const _tweetId = _tweetURL.split("/").pop()?.split("?")[0];
+
+    try {
+      const res = await axios(`/api/getTweet/${_tweetId}`);
+      const { data: resData } = res;
+      let _tweetText = htmlDecode(resData.data.text);
+
+      let _tweetMedia;
+      if (resData.includes) {
+        _tweetMedia = resData.includes.media.filter(
+          (m: { type: "photo" | "video" }) => m.type === "photo"
+        );
+
+        // remove t.co link from tweet text
+        _tweetText = _tweetText.split("https://t.co/").slice(0, -1).join("");
+      }
+
+      console.log({ _tweetText, _tweetMedia });
+      setTweetText(_tweetText);
+      setTweetMedia(_tweetMedia);
+    } catch (e: any) {
+      console.log(e);
     }
 
     setIsFetching(false);
   };
 
+  useEffect(() => {
+    if (tweetURL) {
+      fetchTweetInfoById(tweetURL);
+    }
+  }, [tweetURL]);
+
   return (
     <Flex
       flex={1}
+      pb="2rem"
       flexDir="column"
       justifyContent="flex-start"
       alignItems="center"
